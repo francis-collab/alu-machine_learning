71d4e439e4714a9e60a2332063ffe9110c6c2cf5
francis-collab <f.mutabazi1@alustudent.com>
2026-01-24 06:08:05 -0800

    Add task 11 files

---
+++ math/linear_algebra/11-the_western_exchange.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+"""Module for transposing a NumPy array."""
+
+import numpy as np
+
+
+def np_transpose(matrix):
+    """
+    Transpose a NumPy array.
+
+    Args:
+        matrix (numpy.ndarray): The input array.
+
+    Returns:
+        numpy.ndarray: The transposed array.
+    """
+    return np.transpose(matrix)

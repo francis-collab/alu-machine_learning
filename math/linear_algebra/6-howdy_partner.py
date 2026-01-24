5f271c5b27d152d2d7175ae3de65564b1c20a09b
francis-collab <f.mutabazi1@alustudent.com>
2026-01-24 05:36:40 -0800

    Add task 6 files

---
+++ math/linear_algebra/6-howdy_partner.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+"""Module for concatenating two arrays."""
+
+
+def cat_arrays(arr1, arr2):
+    """
+    Concatenate two arrays.
+
+    Args:
+        arr1 (list): The first array.
+        arr2 (list): The second array.
+
+    Returns:
+        list: A new concatenated array.
+    """
+    return arr1 + arr2
